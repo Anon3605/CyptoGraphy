c92336c133bf9bd5b85d9b45fd0a154a439b94ea
Arafat Sarkar <arafatgovt01@gmail.com>
2025-02-06 01:50:03 +0600

    Commit 06

---
+++ Encoding Scheme/03_formatEncoding/02_hexadecimal.py
@@ -0,0 +1,9 @@
+text=input("Give the text you want to convert in hexadecimal: ")
+print("""As we know there are some title called padding which actually adds
+zeros before the actual hex so that we can differ th characters
+that in the hex string.""")
+padding=int(input("Give the length of each hex values for characters: "))
+x=""
+for i in text:
+    x+="".join(format(ord(i),f"0{padding}x"))
+print(x)
