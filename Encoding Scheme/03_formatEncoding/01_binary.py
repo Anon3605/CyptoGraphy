3a3ed3a9fb9af5c7afd6dd5d21a43330dcb12dd5
Arafat Sarkar <arafatgovt01@gmail.com>
2025-02-06 01:21:16 +0600

    Commit 05

---
+++ Encoding Scheme/03_formatEncoding/01_binary.py
@@ -0,0 +1,9 @@
+text=input("Give the text you want to convert in binary: ")
+print("""As we know there are some title called padding which actually adds
+zeros before the actual binary so that we can differ th characters
+that in the binary string.""")
+padding=int(input("Give the length of each binary values for characters: "))
+x=""
+for i in text:
+    x+="".join(format(ord(i),f"0{padding}b"))
+print(x)
