40a38a1a84fe74eff37002361028a33afe734293
Arafat Sarkar <arafatgovt01@gmail.com>
2025-02-05 11:08:39 +0600

    Commit 03

--- Encoding Scheme/01_text&CharachterBasedEncoding/01_asciiEncoding_01.py
+++ Encoding Scheme/01_text&CharachterBasedEncoding/01_asciiEncoding_01.py
@@ -1,8 +1,9 @@
-get=input()
+get=input("Enter the Text You wanna Encode: ")
 out=""
 for i in range(len(get)):
     temp=str(bin(ord(get[i])))
-    if len(temp)!=8:
+    if len(temp)<8:
         out+="0"*(8-len(temp[2::]))+temp[2::]
-print(len(out))
+    else:
+        print("The Character you gave doesn't belong to the ASCII Table")
 print(out)
--- Encoding Scheme/01_text&CharachterBasedEncoding/01_asciiEncoding_02.py
+++ Encoding Scheme/01_text&CharachterBasedEncoding/01_asciiEncoding_02.py
@@ -1,5 +1,6 @@
-get = input("Enter text: ")
-out = ''.join(format(ord(char), '08b') for char in get)
-
-print(len(out))
+get = input("Enter the Text You wanna Encode: ")
+try:
+    out = ''.join(format(ord(char), '08b') for char in get)
+except:
+    print("The Character you gave doesn't belong to the ASCII Table")
 print(out)
