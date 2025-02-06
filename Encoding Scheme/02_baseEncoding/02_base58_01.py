9c7914838df3cfa0ae3debe4f808cdbe60e77cf5
Arafat Sarkar <arafatgovt01@gmail.com>
2025-02-07 02:28:37 +0600

    Commit 10

--- Encoding Scheme/02_baseEncoding/02_base58_01.py
+++ Encoding Scheme/02_baseEncoding/02_base58_01.py
@@ -1,24 +1,23 @@
-base64 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-     'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-     'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c',
-     'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
-     'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6',
-     '7', '8', '9', '+', '/']
-base64String=""
+base58 = ['1', '2', '3', '4', '5', '6', '7', '8',
+     '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+     'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S',
+     'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b',
+     'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+     'v', 'w', 'x', 'y', 'z']
 
-text = input("Write what ever you want to encode in Base64: ")
-binaryString="".join(format(ord(character), "08b") for character in text)
-idx=0
-for i in range((len(binaryString)//6)+1):
-    temp=binaryString[idx:idx+6]
-    base64String+=base64[int(temp,2)]
-    idx+=6
-remain=len(binaryString)-(idx-6)
-if remain>0:
-    lastElements=int((6-remain)/2)
-    base64String+=base64[int((binaryString[(idx-6):len(binaryString)]+"00"*lastElements),2)]
-    base64String+="="*(lastElements)
-print(base64String)
+def recursion(plainString): #This one just gives you the total value
+    if len(plainString)==1:
+        return ord(plainString[-len(plainString)])*(2**((len(plainString)-1)*8))
+    return ord(plainString[-len(plainString)])*(2**((len(plainString)-1)*8))+recursion(plainString[1::])
 
+def recursion02(total,listFinal): #This one do the modulo operations
+    if total%58==0:
+        return listFinal
+    recursion02(total//58,listFinal)
+    return listFinal.append(total%58)
 
+total,remainderList=recursion(input("Give any text to convert into base64 ma homie: ")),[]
+recursion02(total,remainderList)
+for values in remainderList:
+    print(base58[values],end="")
