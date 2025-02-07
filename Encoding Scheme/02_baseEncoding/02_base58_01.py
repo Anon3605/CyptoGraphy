bec66c9cadbcbdc747800b63a9cad7c1b110337a
Arafat Sarkar <arafatgovt01@gmail.com>
2025-02-07 12:59:18 +0600

    Commit 11

--- Encoding Scheme/02_baseEncoding/02_base58_01.py
+++ Encoding Scheme/02_baseEncoding/02_base58_01.py
@@ -6,18 +6,31 @@ base58 = ['1', '2', '3', '4', '5', '6', '7', '8',
      'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
      'v', 'w', 'x', 'y', 'z']
 
-def recursion(plainString): #This one just gives you the total value
-    if len(plainString)==1:
-        return ord(plainString[-len(plainString)])*(2**((len(plainString)-1)*8))
-    return ord(plainString[-len(plainString)])*(2**((len(plainString)-1)*8))+recursion(plainString[1::])
+# def recursion(plainString): #This one just gives you the total value
+#     if len(plainString)==1:
+#         return ord(plainString[-len(plainString)])*(2**((len(plainString)-1)*8))
+#     return ord(plainString[-len(plainString)])*(2**((len(plainString)-1)*8))+recursion(plainString[1::])
 
-def recursion02(total,listFinal): #This one do the modulo operations
-    if total%58==0:
-        return listFinal
-    recursion02(total//58,listFinal)
-    return listFinal.append(total%58)
+def recursion(plainString):
+    total=0
+    for i in range(len(plainString)):
+        total+=ord(plainString[-(i+1)])*(2**(i*8))
+    print(total)
+    return total
 
-total,remainderList=recursion(input("Give any text to convert into base64 ma homie: ")),[]
+# def recursion02(total,listFinal): #This one do the modulo operations
+#     if total%58==0:
+#         return listFinal
+#     recursion02(total//58,listFinal)
+#     return listFinal.append(total%58)
+
+def recursion02(total,listFinal):
+    while total%58!=0:
+        listFinal.append(total%58)
+        total=total//58
+
+total,remainderList=recursion(input("Give any text to convert into base58 ma homie: ")),[]
 recursion02(total,remainderList)
+remainderList.reverse()
 for values in remainderList:
     print(base58[values],end="")
--- Encoding Scheme/02_baseEncoding/ctf.py
+++ Encoding Scheme/02_baseEncoding/ctf.py
@@ -1,6 +1,11 @@
 import base64
+import sys
 hex_string = "72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf"
 byte_data = bytes.fromhex(hex_string)
 print(byte_data)
 base64_encoded = base64.b64encode(byte_data)
 print(base64_encoded.decode())
+print(sys.maxsize)
+X=12345687946545646495256261646415614564614649879845649846541
+print(X)
+print(sys.getsizeof(X))
