1acd121fb0aee888d4c11d674fb6233804c6f55e
Arafat Sarkar <arafatgovt01@gmail.com>
2025-02-07 19:54:19 +0600

    Commit 12

--- Encoding Scheme/02_baseEncoding/04_base85_01.py
+++ Encoding Scheme/02_baseEncoding/04_base85_01.py
@@ -12,7 +12,6 @@ base85=[
 def asciiBytes():
     hexCode=[hex(character) for character in bytearray
         (input("Enter the text you want to encode in Base85: ").encode())]
-    print(hexCode)
     merger4(hexCode)
 
 def merger4(hexCode,bytesList=[]):
@@ -29,7 +28,6 @@ def merger4(hexCode,bytesList=[]):
             combineHex+=hexCode[-hexC][-1:-3:-1]
         combineHex+="x0"
         bytesList.append(combineHex[-1::-1]+("00"*(4-len(bytesList)%4)))
-    print(bytesList)
     b58encode(bytesList)
 
 def b58encodeOperation(encode):
@@ -38,9 +36,7 @@ def b58encodeOperation(encode):
     n2=int((encode/7225)%85)+33
     n3=int((encode/85)%85)+33
     n4=int((encode/1)%85)+33
-    print(n0,n1,n2,n3,n4)
     return [n0,n1,n2,n3,n4]
-
 
 def b58encode(byteList):
     encodeString=""
